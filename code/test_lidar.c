da89e18315bc42ebd850ce7fdae07d7e7c3ec770
Dylan Brillaud <145108490+Virddav@users.noreply.github.com>
2024-05-22 14:03:33 +0200

    Update test_lidar.c

--- code/test_lidar.c
+++ code/test_lidar.c
@@ -21,7 +21,7 @@ int main() {
 
     // Définir l'adresse et le port
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = inet_addr("192.168.1.200");;
     address.sin_port = htons(PORT);
 
     // Associer le socket à l'adresse et au port
