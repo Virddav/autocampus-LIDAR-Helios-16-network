04194a4604b093ea18b6522b4eadaeed4c83b6cb
habas achraf <habas.achraf@gmail.com>
2024-06-05 10:18:47 +0200

    fix repo

--- src/test_lidar.c
+++ src/test_lidar.c
@@ -8,47 +8,55 @@
 #define PORT 6699
 #define BUFFER_SIZE 1248
 
-long hex_to_decimal(const char *hex_str) {
-    char *endptr;
-    errno = 0;  // Pour distinguer l'erreur de strtol
-    long decimal = strtol(hex_str, &endptr, 16);
+/**
+ *
+ * @file config.c
+ * @brief This file represents a "C" application that catches UDP packet from HELIOS16 to extract and interpret the relevant data
+ *
+ *
+ **/
 
-    // Vérification des erreurs
-    if (errno != 0) {
-        perror("strtol");
-        return -1;
-    }
 
-    if (endptr == hex_str) {
-        fprintf(stderr, "Aucune conversion n'a été effectuée : %s\n", hex_str);
-        return -1;
-    }
 
-    return decimal;
-}
+/**
+ *
+ * @brief This function extracts the time stamp from the UDP packet
+ * @param packet A string of hexadecimal values to extract the time stamp from
+ *
+ **/
 
 void read_header(unsigned char* packet){
 	unsigned char* timestamp = malloc(sizeof(unsigned char) * 10);
 	memcpy(timestamp,packet+20,10);
-	
-      	for (int i=0; i<10;i++){
-      		printf("%02X ",timestamp[i]);
-      	}
+    int t_sec = 0;
+    int t_msec = 0;
+    for (int i=0; i<6; i++){
+            t_sec += (unsigned char)timestamp[i];
+    }
+
+    for (int i=6;i<10;i++){
+        t_msec += (unsigned char) timestamp[i];
+    }
+        printf("Temps en sec : %d\n",t_sec);
+        printf("Temps en microsec : %d\n",t_msec);
 
 }
-    unsigned char packet[1248];
 
-    // Remplir le tableau avec des valeurs hexadécimales (0x00 à 0xFF)
-    for (int i = 0; i < 1248; i++) {
-        packet[i] = (unsigned char)(i % 256); // Les valeurs cyclent de 0x00 à 0xFF
-    }
+
+/**
+ *
+ *@brief This function processes the MSOP packets to extract azimuth from the Data Block
+ * @param packet A string of hexadecimal values to extract the Data Block from
+ *
+ *
+ **/
 
 void read_datablock (unsigned char* packet){
 	for(int y = 0; y<12; y++){
 		unsigned char* azimuth = malloc(sizeof(unsigned char) * 2);
-		memcpy(azimuth,packet+48+2+98*y,2);
+		memcpy(azimuth,packet+42+100*y,2);
         unsigned char* cloudpoint = malloc(sizeof(unsigned char) * 96);
-        memcpy(cloudpoint,packet+48+4+96*y,96);
+        memcpy(cloudpoint,packet+42+4+100*y,96);
 
         printf("Datablock %d : \nAzimuth = %02X%02X\n", y, azimuth[0], azimuth[1]);
 	    for (int i=0; i<96;i++){
@@ -57,6 +65,15 @@ void read_datablock (unsigned char* packet){
 	    printf("\n\n\n");
 	}
 }
+
+/**
+ *
+ *@brief The Main function where the connection is set between the HELIOS16 and the Host, and initiates the reception of the UDP packets.
+ *
+ * :
+ *
+ *
+ **/
 
 int main() {
     int sockfd;
@@ -70,7 +87,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Configurer l'adresse du serveur
+    // Configurer l'adresse du serveurThis function extracts the time stamp from the UDP paquet
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -99,11 +116,12 @@ int main() {
       	//	printf("%02X ",buffer[i]);
       	//}
       	//printf("\n\n");
-      	//read_header(buffer);
-      	read_datablock(buffer);
+      	read_header(buffer);
+      	//read_datablock(buffer);
     }
 
     // Fermer la socket (en réalité, ce point ne sera jamais atteint à cause de la boucle infinie)
     close(sockfd);
     return 0;
 }
+
