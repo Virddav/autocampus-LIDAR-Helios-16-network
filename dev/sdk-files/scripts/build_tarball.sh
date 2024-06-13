20fd10b2898cfda273bd6f68722878adbbda6197
virddav <dbrillaud@outlook.com>
2024-06-13 11:56:44 +0200

    Correction on raw-its.c and add of conf file

--- dev/sdk-files/scripts/build_tarball.sh
+++ dev/sdk-files/scripts/build_tarball.sh
@@ -3,5 +3,6 @@
 cd $(dirname "$0")
 cp ../aoc_makefile/Makefile /home/duser/mk6/stack/apps/exampleETSI/
 cp -r ../src/* /home/duser/mk6/stack/apps/exampleETSI/src/
+cp ../DSRC_RAW/* /home/duser/mk6/stack/apps/exampleETSI/
 make
 mv /home/duser/mk6/stack/apps/exampleETSI/exampleETSI-mk6-Exported.tgz ../
